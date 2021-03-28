96cdf035b3dc72fba8a18b6280a6abbb37817362
k906506 <k906506@naver.com>
2021-03-28 22:54:53 +0900

    Average Filter

---
+++ 3 Week/1. Average Filter.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+
+def my_average_filter_3x3(src):
+    mask = np.full((3, 3), 1/9)
+    dst = cv2.filter2D(src, -1, mask)
+    return dst
+
+if __name__ == "__main__":
+    src = cv2.imread("Lena.png", cv2.IMREAD_GRAYSCALE)
+    dst = my_average_filter_3x3(src)
+
+    cv2.imshow("original", src)
+    cv2.imshow("average filter", dst)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
